43113d16318caf6b43a4bdb3b9bb8cde8663b9d1
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2023-12-17 16:19:20 +0100

    adding CodingProblem20

---
+++ CodingProblem20/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] tab = {100, 4, 200, 1, 3, 2 };
+        Console.WriteLine(LongestConsecutiveElements(tab));
+    }
+    static int LongestConsecutiveElements(int[] tab)
+    {
+        int holder, counter;
+        int longestConsecutive = 0;
+        for (int i = 0; i<tab.Length; i++)
+        {
+            counter = 1;
+            holder = tab[i];
+            for (int j = 0; j<tab.Length; j++)
+            {
+                if (tab[j] == holder + 1)
+                {
+                    counter++;
+                    j = -1;
+                    holder++;
+                }
+            }
+            if (counter > longestConsecutive)
+            {
+                longestConsecutive = counter;
+            }
+        }
+        return longestConsecutive;
+    }
+}
