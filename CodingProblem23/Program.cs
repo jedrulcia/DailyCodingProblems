1ce7f834e4f153e67d026b0197846a412c7691c9
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2023-12-23 18:45:53 +0100

    adding cp23

---
+++ CodingProblem23/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] tab = { 1, 2, 4, 4, 6 };
+        int n = 9;
+        int[] contiguousElements = ContiguousElements(tab, n);
+
+        if (contiguousElements != null )
+        {
+            Array.ForEach(contiguousElements, Console.WriteLine);
+        }
+        else
+        {
+            Console.WriteLine($"There is no such sequence which add up to {n}");
+        }
+    }
+    static int[] ContiguousElements(int[] tab, int n)
+    {
+        for (int i = 0; i< tab.Length; i++)
+        {
+            int[] contiguousElements = new int[0];
+            int sum = 0;
+            int x = 0;
+            for (int j = i + 1; j < tab.Length; j++)
+            {
+                x++;
+                Array.Resize(ref contiguousElements, x);
+                contiguousElements[x-1] = tab[j];
+                sum = sum + tab[j];
+                if (sum == n)
+                {
+                    return contiguousElements;
+                }
+                else if (sum > n)
+                {
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+}
