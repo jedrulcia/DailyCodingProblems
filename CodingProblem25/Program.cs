1fcb60ccfabe9954a9ba24ac6bb65208057f705f
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-01-04 19:43:39 +0100

    adding cd25

---
+++ CodingProblem25/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] tab = { -9, -2, 0, 2, 3 };
+
+        SquareElements(tab);
+        SortTheTab(tab);
+    }
+
+    static void SortTheTab(int[] tab)
+    {
+        for (int i = 0; i < tab.Length; i++)
+        {
+            for (int j = i + 1; j < tab.Length; j++)
+            {
+                if (tab[i] > tab[j])
+                {
+                    int holder = tab[i];
+                    tab[i] = tab[j];
+                    tab[j] = holder;
+                }
+            }
+        }
+    }
+
+    static void SquareElements(int[] tab)
+    {
+        for (int i = 0; i < tab.Length; i++)
+        {
+            tab[i] = tab[i] * tab[i];
+        }
+    }
+}
