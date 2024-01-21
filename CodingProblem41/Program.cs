fac7d8cb87119ac2f30adf2ad39628c634b616b5
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-01-21 20:52:18 +0100

    adding cd41

---
+++ CodingProblem41/Program.cs
@@ -0,0 +1,51 @@
+﻿class Program
+{
+    static void Main()
+    {
+        BinaryTree root = new BinaryTree(10);
+        root.left = new BinaryTree(5);
+        root.left.right = new BinaryTree(2);
+        root.right = new BinaryTree(5);
+        root.right.right = new BinaryTree(1);
+        root.right.right.left = new BinaryTree(-1);
+
+        int sum = int.MaxValue;
+        BinaryTree.MinimumPathSum(root, ref sum, 0);
+
+        Console.WriteLine(sum);
+
+    }
+}
+class BinaryTree
+{
+    public int value;
+    public BinaryTree left;
+    public BinaryTree right;
+
+    public BinaryTree (int value)
+    {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public static void MinimumPathSum(BinaryTree root, ref int sum, int currentSum)
+    {
+        currentSum += root.value;
+        if (root.left != null)
+        {
+            BinaryTree.MinimumPathSum(root.left, ref sum, currentSum);
+        }
+        if (root.right != null)
+        {
+            BinaryTree.MinimumPathSum(root.right, ref sum, currentSum);
+        }
+        if (root.left == null && root.right == null)
+        {
+            if (currentSum < sum)
+            {
+                sum = currentSum;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -117,3 +117,12 @@ Given an array of numbers representing the stock prices of a company in chronolo
 
 CodingProblem39:
 Create a function that creates a pattern as a 2D array for a given number.
+
+CodingProblem40:
+Design and implement a HitCounter class that keeps track of requests (or hits). It should support the following operations:
+record(timestamp): records a hit that happened at timestamp
+total(): returns the total number of hits recorded
+range(lower, upper): returns the number of hits that occurred between timestamps lower and upper (inclusive)
+
+CodingProblem41:
+Given a binary tree, find a minimum path sum from root to a leaf.
