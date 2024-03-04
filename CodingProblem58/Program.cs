e461d790fe24d592fec13d5415b499ed8b06aff5
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-03-04 22:51:10 +0100

    adding cd 58

---
+++ CodingProblem58/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        WhichGameShouldAlicePlay();
+    }
+    static int RollDice(int firstValue, int followingValue)
+    {
+        Random random = new Random();
+        bool isConditionMet = false;
+        int firstHolder = 0, secondHolder = 0, counter = 0;
+
+        while (isConditionMet == false)
+        {
+            counter++;
+            firstHolder = secondHolder;
+            secondHolder = random.Next(1, 7);
+            if (firstHolder == firstValue && secondHolder == followingValue) isConditionMet = true;
+        }
+        return counter;
+    }
+    static void WhichGameShouldAlicePlay()
+    {
+        int firstSumOfPayments = 0;
+        int secondSumOfPayments = 0;
+
+        for (int i = 0; i < 10000; i++)
+        {
+            firstSumOfPayments += RollDice(5, 6);
+            secondSumOfPayments += RollDice(5, 5);
+        }
+        int averageFirstPayments = firstSumOfPayments / 10000;
+        int averageSecondPayments = secondSumOfPayments / 10000;
+        string aliceShouldPlay = "";
+
+        if (averageFirstPayments < averageSecondPayments) aliceShouldPlay = "first game";
+        else if (averageFirstPayments > averageSecondPayments) aliceShouldPlay = "second game";
+        else aliceShouldPlay = "it does not matter";
+
+        Console.WriteLine($"Alice should play: {aliceShouldPlay}.");
+    }
+}
--- README.md
+++ README.md
@@ -175,3 +175,6 @@ Given an array of integers, return a new array where each element in the new arr
 
 CodingProblem57:
 Given an N by N matrix, rotate it by 90 degrees clockwise.
+
+CodingProblem58:
+Alice wants to join her school's Probability Student Club. Membership dues are computed via one of two simple probabilistic games. The first game: roll a die repeatedly. Stop rolling once you get a five followed by a six. Your number of rolls is the amount you pay, in dollars. The second game: same, except that the stopping condition is a five followed by a five. Which of the two games should Alice elect to play? Does it even matter? Write a program to simulate the two games and calculate their expected value.
