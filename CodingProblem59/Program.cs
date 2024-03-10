a0ce16921741833fd74cdac389ac26a0d37ab5e2
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-03-10 21:01:56 +0100

    adding cd59

---
+++ CodingProblem59/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] tab = { 42, 56, 14 };
+        int greatestDenominator = FindGreatestDenominator(tab);
+        Console.WriteLine(greatestDenominator);
+    }
+    static int FindGreatestDenominator(int[] tab)
+    {
+        int smallestNumber = FindSmallestNumber(tab);
+        bool shouldContinue = true;
+
+        while (shouldContinue)
+        {
+            shouldContinue = false;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] % smallestNumber != 0)
+                {
+                    shouldContinue = true;
+                    smallestNumber -= 1;
+                    break;
+                }
+            }
+        }
+        return smallestNumber;
+    }
+    static int FindSmallestNumber(int[] tab)
+    {
+        int smallestNumber = tab[0];
+
+        for (int i = 0; i < tab.Length; i++) if (tab[i] < smallestNumber) smallestNumber = tab[i];
+
+        return smallestNumber;
+    }
+}
