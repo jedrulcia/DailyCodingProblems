303bd714fa3b22f24b5ee9ed90eadb4e88d7c4e1
Jedrzej Koriat <jedrzej.koriat@gmail.com>
2024-04-22 16:19:50 +0200

    adding cd65

---
+++ CodingProblem65/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 3, 10 };
+        Console.WriteLine(FindSmallestMissingPositive(arr));
+    }
+    static int FindSmallestMissingPositive(int[] arr)
+    {
+        int smallestMissing = 1;
+        for (int i = 0; i < arr.Length && arr[i] <= smallestMissing; i++) smallestMissing += arr[i];
+        return smallestMissing;
+    }
+}
